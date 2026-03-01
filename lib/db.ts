69cc692fbbd2274965a32bc7622ea1d4233a2ee5
kothavitesh <2410030005@klh.edu.in>
2026-03-01 19:15:43 +0530

    Refactor Prisma client initialization for global scope

--- lib/db.ts
+++ lib/db.ts
@@ -1,13 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined
 }
 
 export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ['error'],
-  })
+  globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+if (process.env.NODE_ENV !== 'production')
+  globalForPrisma.prisma = prisma
